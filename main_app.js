46ff423e958ae3869244805e7b083ef906d514e3
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-03-07 12:02:38 +0100

    Put the files on my remote

---
+++ main_app.js
@@ -0,0 +1,12 @@
+// This is a single-line comment.
+
+/* Here's a
+multi-line
+comment. */
+
+// console.log('This code is no longer active')
+
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
